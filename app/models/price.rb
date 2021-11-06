07b6ae9f7692151166ca553260e5b8fc286a5628
efgalvao <solfieri@hotmail.com>
2021-11-06 14:23:50 -0300

    Adjust prices

---
+++ app/models/price.rb
@@ -0,0 +1,19 @@
+class Price < ApplicationRecord
+  belongs_to :stock, touch: true
+
+  before_create :set_date
+
+  scope :past_date, ->(date) { where('date <= ?', date) }
+
+  monetize :price_cents
+
+  def self.monthly_price
+    group_by_month(:date, last: 12, current: true).average('price')
+  end
+
+  private
+
+  def set_date
+    self.date = DateTime.current unless date
+  end
+end
