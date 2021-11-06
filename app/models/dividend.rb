f6d433a548030de888cc859d3dcc2a01cdb4fbc3
efgalvao <solfieri@hotmail.com>
2021-11-06 14:06:46 -0300

    Adjust dividends section

---
+++ app/models/dividend.rb
@@ -0,0 +1,17 @@
+class Dividend < ApplicationRecord
+  belongs_to :stock, touch: true
+
+  before_create :set_date
+
+  monetize :value_cents
+
+  def self.monthly_dividend
+    group_by_month(:date, last: 12, current: true).maximum('value')
+  end
+
+  private
+
+  def set_date
+    self.date = DateTime.current unless date
+  end
+end
---
+++ app/views/dividends/_dividend.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! dividend, :id, :date, :value, :stock_id, :created_at, :updated_at
+json.url dividend_url(dividend, format: :json)
