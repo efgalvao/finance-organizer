2d07e2cb2ff68f3e1490598295889995ebc131e4
efgalvao <solfieri@hotmail.com>
2021-11-18 15:12:48 -0300

    Remove last_balance helper

--- app/helpers/balances_helper.rb
+++ app/helpers/balances_helper.rb
@@ -1,12 +1,2 @@
 module BalancesHelper
-  def last_balance(account_id)
-    # account = Account.find(account_id)
-
-    # account.balances.current.first&.updated_at.nil? ? account.create_balance : account.update_balance
-
-    # account.generate_balance if account.balances.current.first&.updated_at.nil? ||
-    #                             account.balances.current.first.updated_at < account.updated_at
-
-    Account.find(account_id).balances.last.balance
-  end
 end
