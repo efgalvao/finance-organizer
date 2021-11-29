706fdf7a39a06ae17edc3b6dbbfb24652aff5ca5
efgalvao <solfieri@hotmail.com>
2021-11-29 09:23:33 -0300

    Create transaction together with dividend

--- app/models/dividend.rb
+++ app/models/dividend.rb
@@ -1,17 +1,28 @@
 class Dividend < ApplicationRecord
   belongs_to :stock, touch: true
 
-  before_create :set_date
+  before_create :set_date, :create_transaction
 
   monetize :value_cents
 
-  def self.monthly_dividend
-    group_by_month(:date, last: 12, current: true).maximum('value')
-  end
+  # def self.monthly_dividend
+  #   group_by_month(:date, last: 12, current: true).maximum('value')
+  # end
 
   private
 
   def set_date
     self.date = DateTime.current unless date
+  end
+
+  def create_transaction
+    Transaction.create(
+      account: stock.account,
+      category_id: Category.find_by(name: 'Dividends').id,
+      value_cents: (value_cents * stock.shares.count),
+      kind: 'income',
+      title: "#{stock.name} Dividend",
+      date: date
+    )
   end
 end
