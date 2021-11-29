d5848d54b88f5c018ec54ab25d08f3d5de5bae6a
efgalvao <solfieri@hotmail.com>
2021-11-29 09:46:17 -0300

    Set first price buying a share

--- app/models/share.rb
+++ app/models/share.rb
@@ -1,7 +1,7 @@
 class Share < ApplicationRecord
   belongs_to :stock, touch: true
 
-  before_create :set_aquisition_date
+  before_create :set_aquisition_date, :set_first_price
 
   scope :bought_this_month, lambda {
                               where('aquisition_date BETWEEN ? AND ?',
@@ -13,19 +13,29 @@ class Share < ApplicationRecord
 
   monetize :aquisition_value_cents
 
-  def self.average_price
-    average('aquisition_value')
-  end
+  # def self.average_price
+  #   average('aquisition_value_cents')
+  # end
 
-  def self.month_qty
-    group_by_month(:aquisition_date, last: 12, current: true).count.each_with_object({}) do |(date, total), cumulative|
-      cumulative[date] = total + (cumulative.prices.last || 0)
-    end
-  end
+  # def self.month_qty
+  #   group_by_month(:aquisition_date, last: 12, current: true).count.each_with_object({}) do |(date, total), cumulative|
+  #     cumulative[date] = total + (cumulative.prices.last || 0)
+  #   end
+  # end
 
   private
 
   def set_aquisition_date
     self.aquisition_date = DateTime.current unless aquisition_date
+  end
+
+  def set_first_price
+    return unless Price.find_by(date: aquisition_date).nil?
+
+    Price.create(
+      date: aquisition_date,
+      price_cents: aquisition_value_cents,
+      stock: stock
+    )
   end
 end
