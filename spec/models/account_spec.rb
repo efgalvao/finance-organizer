977b53261dcd4bbaff4ebf673a8a24f7d1f54683
efgalvao <solfieri@hotmail.com>
2021-12-28 09:34:42 -0300

    test: account models specs

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
+  let(:account) { create(:account, balance: 100) }
   describe 'associations' do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to have_many(:transactions) }
@@ -10,5 +11,41 @@ RSpec.describe Account, type: :model do
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
+  end
+
+  describe "#total_invested" do
+    let(:stock1) { create(:stock, account: account) }
+    let!(:share1) { create_list(:share, 2, stock: stock1, aquisition_value: 100) }
+    let(:stock2) { create(:stock, account: account) }
+    let!(:share2) { create_list(:share, 2, stock: stock2, aquisition_value: 100) }
+
+
+    it 'returns the total invested in the account' do
+      expect(account.total_invested.fractional).to eq(40000)
+    end
+  end
+
+  describe "#total_stock_value" do
+    let(:stock1) { create(:stock, account: account) }
+    let!(:share1) { create_list(:share, 2, stock: stock1, aquisition_value: 100) }
+    let(:stock2) { create(:stock, account: account) }
+    let!(:share2) { create_list(:share, 2, stock: stock2, aquisition_value: 100) }
+
+
+    it 'returns the total invested in the account' do
+      expect(account.total_stock_value.fractional).to eq(40000)
+    end
+  end
+  
+  describe "#total_stock_value" do
+    let(:stock1) { create(:stock, account: account) }
+    let!(:share1) { create_list(:share, 2, stock: stock1, aquisition_value: 100) }
+    let(:stock2) { create(:stock, account: account) }
+    let!(:share2) { create_list(:share, 2, stock: stock2, aquisition_value: 100) }
+
+
+    it 'returns the total invested in the account' do
+      expect(account.stock_plus_balance.fractional).to eq(50000)
+    end
   end
 end
