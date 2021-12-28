7fcc9a96c7687e8158510e24052bba7794dca57d
efgalvao <solfieri@hotmail.com>
2021-12-28 15:53:13 -0300

    refactor: stock methods

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -7,49 +7,37 @@ class Stock < ApplicationRecord
   validates :name, presence: true
   validates :account, presence: true
 
+  def name_with_account
+    "#{name} (#{account.name})"
+  end
+
+  def current_price
+    return 0 if prices.empty?
+
+    prices.order('date desc').first&.price
+  end
+
   def total_invested
     Money.new(shares.sum(:aquisition_value_cents))
   end
 
   def average_aquisition_price
     return 0 if shares.count.zero?
+
     total_invested / shares.count
-  end
-
-  def updated_balance
-    price = if prices.order('date desc').first.nil?
-              shares.order('aquisition_date desc').first&.aquisition_value
-            else
-              prices.order('date desc').first&.price
-            end
-    return 0 if price.nil?
-
-    (price * shares.count).round(2)
-  end
-
-  def individual_price
-    price = if prices.order('date desc').first.nil?
-              shares.order('aquisition_date desc').first&.aquisition_value
-            else
-              prices.order('date desc').first&.price
-            end
-    return 0 if price.nil?
-
-    price.round(2)
-  end
-
-  def current_price
-    if prices.order('date desc').first&.price.nil?
-      shares.order('aquisition_date desc').first&.aquisition_value
-    else
-      prices.order('date desc').first&.price
-    end
   end
 
   def total_current_price
     return 0 if current_price.nil?
 
     current_price * shares.count
+  end
+
+  def updated_balance
+    return 0 if prices.empty?
+
+    price = prices.order('date desc').first&.price
+    price * shares.count
   end
 
   def monthly_price
@@ -71,7 +59,8 @@ class Stock < ApplicationRecord
   def monthly_dividends_total
     grouped_dividends = {}
     dividends.each do |dividend|
-      grouped_dividends[dividend.date.strftime('%B/%Y').to_s] = dividend.value.to_f * shares.where('aquisition_date <= ?', dividend.date).count
+      grouped_dividends[dividend.date.strftime('%B/%Y').to_s] =
+        dividend.value.to_f * shares.where('aquisition_date <= ?', dividend.date).count
     end
     grouped_dividends
   end
@@ -84,9 +73,5 @@ class Stock < ApplicationRecord
             end
     shares_count = shares.past_date(date).count
     price * shares_count
-  end
-
-  def name_with_account
-    "#{name} (#{account.name})"
   end
 end
