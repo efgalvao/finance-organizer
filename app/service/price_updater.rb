42185bef1e4c59a9ebc8d3fa3f7f419518bb363a
efgalvao <solfieri@hotmail.com>
2022-01-10 18:33:17 -0300

    refactor: get_current_price

--- app/service/price_updater.rb
+++ app/service/price_updater.rb
@@ -2,6 +2,10 @@ class PriceUpdater
   def self.get_price(ticker)
     url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{ticker}.SAO&apikey=#{ENV['ALPHAVANTAGE_API_KEY']}"
     response = RestClient.get(url)
+    puts('-------------------> ', response.body)
+    json = JSON.parse(response)
+    json['Global Quote']['05. price']
+    puts('-------------------> ', json)
     json = JSON.parse response
     json['Global Quote']['05. price'].to_f.round(2)
   end
