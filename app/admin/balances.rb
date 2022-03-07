0f3083276f3880de07e11849fdbdd77c33d9d431
efgalvao <61836657+efgalvao@users.noreply.github.com>
2022-03-07 17:44:20 -0300

    Add ActiveAdmin (#16)

---
+++ app/admin/balances.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register Balance do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :balance_cents, :date, :account_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:balance_cents, :date, :account_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+end
---
+++ app/admin/dividends.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register Dividend do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :date, :value_cents, :stock_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:date, :value_cents, :stock_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+end
