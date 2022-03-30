b8673c806591471d215605aa7b7e66f91d595b94
efgalvao <61836657+efgalvao@users.noreply.github.com>
2022-03-30 12:19:18 -0300

    feat: add invested column (#19)

--- app/services/statements/create_incomes_expenses.rb
+++ app/services/statements/create_incomes_expenses.rb
@@ -16,10 +16,11 @@ module Statements
       table = {}
       (0..6).each do |i|
         date = Time.zone.today - i.month
-        data = { incomes: 0, expenses: 0, total: 0 }
+        data = { incomes: 0, expenses: 0, invested: 0, total: 0 }
         user.accounts.map do |account|
           data[:incomes] += account.incomes(date)
           data[:expenses] += account.expenses(date)
+          data[:invested] += account.invested(date)
           data[:total] += account.total_balance(date)
         end
         table[date.strftime('%B, %Y').to_s] = data
