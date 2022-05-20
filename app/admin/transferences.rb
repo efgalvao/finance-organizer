fadc87a194ff922fce0f28ed66d083d18f6b61bf
efgalvao <solfieri@hotmail.com>
2022-05-19 22:48:07 -0300

    update activeadmin models

--- app/admin/transferences.rb
+++ app/admin/transferences.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register Transference do
+ActiveAdmin.register Account::Transference do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
