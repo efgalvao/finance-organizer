4b9a2c210bf3b4fb6a1ecdab71649686a49aeb94
efgalvao <solfieri@hotmail.com>
2022-05-26 22:47:01 -0300

    fix categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,40 +11,27 @@ class CategoriesController < ApplicationController
 
   def create
     @category = Category.new(category_params)
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to categories_path, notice: 'Category successfully created.' }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    if @category.save
+      redirect_to categories_path, notice: 'Category successfully created.'
+    else
+      render :new
     end
   end
 
   def update
     authorize @category
 
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to @category, notice: 'Category successfully updated.' }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    if @category.update(category_params)
+      redirect_to categories_path, notice: 'Category successfully updated.'
+    else
+      render :edit
     end
   end
 
   def destroy
     authorize @category
 
-    respond_to do |format|
-      if @category.destroy
-        format.html { redirect_to categories_path, notice: 'Category successfully removed.' }
-        format.json { head :no_content }
-      end
-    end
+    redirect_to categories_path, notice: 'Category successfully removed.' if @category.destroy
   end
 
   def edit
