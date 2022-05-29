44345cb763987fcef96d8e5aee24adc9862a4d71
efgalvao <solfieri@hotmail.com>
2022-05-29 14:46:14 -0300

    fix price updater response

--- app/services/investments/stock/price_updater.rb
+++ app/services/investments/stock/price_updater.rb
@@ -4,8 +4,8 @@ module Investments
       def self.get_price(ticker)
         url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=#{ticker}.SAO&apikey=#{ENV['ALPHAVANTAGE_API_KEY']}"
         response = RestClient.get(url)
-        json = JSON.parse response
-        { price: json['Global Quote']['05. price'].to_f.round(2),
+        json = JSON.parse(response)
+        { value: json['Global Quote']['05. price'].to_f.round(2),
           date: json['Global Quote']['07. latest trading day'] }
       end
     end
