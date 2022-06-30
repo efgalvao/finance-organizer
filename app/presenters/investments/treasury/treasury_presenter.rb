60387d71f6c25502203268e5eab157a028cbfb09
efgalvao <solfieri@hotmail.com>
2022-06-29 21:09:05 -0300

    add chart for treasuries

---
+++ app/presenters/investments/treasury/treasury_presenter.rb
@@ -0,0 +1,21 @@
+module Investments
+  module Treasury
+    class TreasuryPresenter < Oprah::Presenter
+      presents_one :account
+
+      def last_semester_positions
+        grouped_positions = {}
+        semester_positions.each do |position|
+          grouped_positions[position.date.strftime('%B %d, %Y').to_s] = position.amount.to_f
+        end
+        grouped_positions
+      end
+
+      private
+
+      def semester_positions
+        positions.where('date > ?', Time.zone.today - 6.months).order(date: :asc)
+      end
+    end
+  end
+end
