145b64f3b1ee49999911190f930f4abc52d2d40b
efgalvao <solfieri@hotmail.com>
2022-07-31 20:58:35 -0300

    fix parcels

--- app/services/transactions/process_credit_transaction.rb
+++ app/services/transactions/process_credit_transaction.rb
@@ -32,7 +32,7 @@ module Transactions
         account_id: params.fetch(:account_id),
         kind: params.fetch(:kind),
         value: params.fetch(:value).to_f / parcels,
-        date: date + parcel.months
+        date: Date.strptime(date, '%Y-%m-%d') + parcel.months
       }
     end
 
