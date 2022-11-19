074786a394ffb667bd2b700f02f6f94f6c677abe
efgalvao <solfieri@hotmail.com>
2022-11-18 22:32:54 -0300

    add hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,7 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  config.hosts << "finance-organizer-production.up.railway.app"
 
   # Do not eager load code on boot.
   config.eager_load = false
