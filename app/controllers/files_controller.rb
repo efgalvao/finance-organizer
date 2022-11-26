a731177b905d2dc94e15705487ab45e53c469617
efgalvao <solfieri@hotmail.com>
2022-11-25 21:58:52 -0300

    add rescue in files controller

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -5,5 +5,7 @@ class FilesController < ApplicationController
     Files::ProcessFile.call(params[:file], current_user.id)
 
     redirect_to user_summary_path, notice: 'File uploaded succesfully.'
+  rescue StandardError
+    redirect_to user_summary_path, notice: 'File not uploaded'
   end
 end
