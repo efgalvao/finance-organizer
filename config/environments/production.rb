4e8a3edd9ffa951559973aca32eff8fa51f31d42
efgalvao <solfieri@hotmail.com>
2022-12-20 23:16:02 -0300

    secret

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
