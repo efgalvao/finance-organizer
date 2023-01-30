e3e7711308bdd52bf705c559932f496d7f7c0b14
efgalvao <solfieri@hotmail.com>
2023-01-29 21:56:45 -0300

    refactor positions_controller

--- app/services/investments/treasury/create_position.rb
+++ app/services/investments/treasury/create_position.rb
@@ -3,10 +3,7 @@ module Investments
     class CreatePosition < ApplicationService
       def initialize(params)
         @params = params
-        @date = set_date
-        @invested = params.fetch(:invested, 0)
         @treasury = Investments::Treasury::Treasury.find(params[:treasury_id])
-        @amount = params.fetch(:amount, 0)
       end
 
       def self.call(params)
@@ -14,27 +11,59 @@ module Investments
       end
 
       def call
-        create_position(params)
+        create_position
       end
 
       private
 
-      attr_reader :params, :date, :invested, :amount, :treasury
+      attr_reader :params, :treasury
 
-      def create_position(params)
+      def create_position
         ActiveRecord::Base.transaction do
-          Investments::Treasury::Position.create(position_params)
-          Investments::Treasury::UpdateTreasury.call(params)
+          position = Investments::Treasury::Position.create!(position_params)
+          Investments::Treasury::UpdateTreasury.call(update_treasury_params)
+          position
         end
       end
 
       def position_params
-        { treasury_id: treasury.id,
-          date: date, amount: amount.to_f.zero? ? set_amount : amount }
+        {
+          treasury_id: treasury.id,
+          date: set_date,
+          amount: set_new_position_value
+        }
       end
 
-      def set_amount
-        (treasury.current_value_cents + invested.to_f * 100) / 100
+      def update_treasury_params
+        {
+          treasury_id: treasury.id,
+          date: set_date,
+          updated_invested: set_new_invested_value,
+          updated_current: set_new_current_value,
+          shares: params[:shares]
+        }
+      end
+
+      def to_cents(value)
+        (value.to_f * 100).to_i
+      end
+
+      def set_new_position_value
+        return params[:amount] if params[:amount].present?
+
+        params.fetch(:invested, 0).to_f + treasury.current_value_cents / 100.0
+      end
+
+      def set_new_invested_value
+        return treasury.invested_value_cents if params[:amount].present?
+
+        to_cents(params.fetch(:invested, 0)) + treasury.invested_value_cents
+      end
+
+      def set_new_current_value
+        return to_cents(params[:amount]) if params[:amount].present?
+
+        to_cents(params.fetch(:invested, 0)) + treasury.current_value_cents
       end
 
       def set_date
